e925a596db247031492ebe1b23b81608fd2e17ca
Zinnia Kar <zinniakar@gmail.com>
2026-08-01 17:14:55 +0530

    Time: 9 ms (35.07%), Space: 13.5 MB (18.42%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> roman = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1 && roman[s[i]] < roman[s[i + 1]]) {
+                result -= roman[s[i]];
+            }
+            else {
+                result += roman[s[i]];
+            }
+        }
+        return result;
+    }
+};
